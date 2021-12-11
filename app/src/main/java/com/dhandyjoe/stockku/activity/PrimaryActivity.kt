8d197b177d8c4b9d503ca28e92e70aa2fc405874
dhandyjoe <dhanjoenkp@gmail.com>
2021-12-11 17:16:51 +0700

    adjust value

--- app/src/main/java/com/dhandyjoe/stockku/activity/PrimaryActivity.kt
+++ app/src/main/java/com/dhandyjoe/stockku/activity/PrimaryActivity.kt
@@ -20,7 +20,7 @@ class PrimaryActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_controller)
 
         val appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            R.id.navigation_transaction, R.id.navigation_item, R.id.navigation_user
         ).build()
 
         setupActionBarWithNavController(navController, appBarConfiguration)
