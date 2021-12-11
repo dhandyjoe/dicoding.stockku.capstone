ff106f202573e89d5b53b387f8b11e344fa290f0
dhandyjoe <dhanjoenkp@gmail.com>
2021-12-11 13:26:33 +0700

    create function search for transaction

--- app/src/main/java/com/dhandyjoe/stockku/activity/TransactionActivity.kt
+++ app/src/main/java/com/dhandyjoe/stockku/activity/TransactionActivity.kt
@@ -1,9 +1,12 @@
 package com.dhandyjoe.stockku.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.dhandyjoe.stockku.adapter.StockAdapter
 import com.dhandyjoe.stockku.adapter.TransactionAdapter
 import com.dhandyjoe.stockku.databinding.ActivityTransactionBinding
 import com.dhandyjoe.stockku.model.Item
@@ -12,6 +15,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 class TransactionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTransactionBinding
     private val firebaseDB = FirebaseFirestore.getInstance()
+    private val listItemSearch = ArrayList<Item>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,15 +38,55 @@ class TransactionActivity : AppCompatActivity() {
             }
 
             if (user.size > 0) {
-                binding.tvStatusNoData.visibility = View.GONE
-                binding.rvTransactionItem.layoutManager = LinearLayoutManager(this)
-                val data = TransactionAdapter(user)
-                binding.rvTransactionItem.adapter = data
-                binding.rvTransactionItem.visibility = View.VISIBLE
+                showRecycleView(user)
             } else {
                 binding.tvStatusNoData.visibility = View.VISIBLE
                 binding.rvTransactionItem.visibility = View.GONE
             }
+
+            searchItem(user)
         }
+    }
+
+    private fun showRecycleView(data: ArrayList<Item>) {
+        binding.tvStatusNoData.visibility = View.GONE
+        binding.rvTransactionItem.layoutManager = LinearLayoutManager(this)
+        val data = StockAdapter(data)
+        binding.rvTransactionItem.adapter = data
+        binding.rvTransactionItem.visibility = View.VISIBLE
+
+        data.setOnItemClickCallback(object : StockAdapter.OnItemClickCallback{
+            override fun onItemClicked(data: Item) {
+                val intent = Intent(this@TransactionActivity, EditItemActivity::class.java)
+                intent.putExtra(EditItemActivity.EXTRA_BARANG, data)
+                startActivity(intent)
+            }
+        })
+    }
+
+    private fun searchItem(data: ArrayList<Item>) {
+        binding.svItem.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                listItemSearch.clear()
+                data.forEach {
+                    if (it.name.toLowerCase().contains(query!!.toLowerCase())) {
+                        listItemSearch.add(it)
+                        showRecycleView(listItemSearch)
+                    }
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                listItemSearch.clear()
+                data.forEach {
+                    if (it.name.toLowerCase().contains(newText!!.toLowerCase())) {
+                        listItemSearch!!.add(it)
+                        showRecycleView(listItemSearch)
+                    }
+                }
+                return false
+            }
+        })
     }
 }
