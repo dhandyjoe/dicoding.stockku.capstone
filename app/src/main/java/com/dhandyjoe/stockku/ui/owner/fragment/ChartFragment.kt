81e8cb7617e793ed4ad09b0c98b2f5ed9889befc
dhandyjoe <dhanjoenkp@gmail.com>
2022-03-22 21:18:04 +0700

    fix ui graphic owner

--- app/src/main/java/com/dhandyjoe/stockku/ui/owner/fragment/ChartFragment.kt
+++ app/src/main/java/com/dhandyjoe/stockku/ui/owner/fragment/ChartFragment.kt
@@ -1,21 +1,21 @@
 package com.dhandyjoe.stockku.ui.owner.fragment
 
-import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.dhandyjoe.stockku.R
 import com.dhandyjoe.stockku.databinding.FragmentChartBinding
-import com.dhandyjoe.stockku.databinding.FragmentTransactionOwnerBinding
-import com.dhandyjoe.stockku.utils.AxisDateFormatter
-import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
+
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -25,7 +25,7 @@ class ChartFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     private lateinit var binding: FragmentChartBinding
-    private var monthValue = ArrayList<String>()
+    val xAxisLabel: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,11 +41,6 @@ class ChartFragment : Fragment() {
     ): View? {
         binding = FragmentChartBinding.inflate(inflater, container, false)
 
-        monthValue.add("Januari")
-        monthValue.add("Februari")
-        monthValue.add("Maret")
-        monthValue.add("April")
-
         barChart()
 
         return binding.root
@@ -59,6 +54,7 @@ class ChartFragment : Fragment() {
         legend.orientation = Legend.LegendOrientation.VERTICAL
         legend.setDrawInside(false)
 
+
         val arrayList = ArrayList<BarEntry>()
         arrayList.add( BarEntry(0f, 150F) )
         arrayList.add( BarEntry(1f, 80F) )
@@ -70,13 +66,45 @@ class ChartFragment : Fragment() {
         barDataSet.color = R.color.second
 
         binding.barChart.description.isEnabled = false
-        binding.barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        binding.barChart.setFitBars(true)
         binding.barChart.data = BarData( barDataSet )
         binding.barChart.animateXY(100, 500)
         binding.barChart.setDrawGridBackground( false )
+
+        binding.barChart.axisRight.isEnabled = false
+
+
+        binding.barChart.xAxis.setDrawGridLines(false)
+        binding.barChart.xAxis.setDrawAxisLine(false)
+
+
+        xAxisLabel.add("Senin")
+        xAxisLabel.add("Selasa")
+        xAxisLabel.add("Rabu")
+        xAxisLabel.add("Kamis")
+
+//
+        val xAxis: XAxis = binding.barChart.xAxis
+        xAxis.valueFormatter = MyAxisFormatter()
+
+        binding.barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        binding.barChart.xAxis.setDrawLabels(true)
+        xAxis.granularity = 1f
 //
 //        val monthArray = AxisDateFormatter (monthValue.toArray(arrayOfNulls(monthValue.size)))
 //        binding.barChart.xAxis.valueFormatter = monthArray
 
     }
+
+    inner class MyAxisFormatter : IndexAxisValueFormatter() {
+        override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+            val index = value.toInt()
+            return if (index < xAxisLabel.size) {
+                xAxisLabel[index]
+            } else {
+                ""
+            }
+        }
+    }
+
 }
